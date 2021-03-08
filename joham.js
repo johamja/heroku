cf514172d36f0183286918b7bc11032b94d44d61
johamja <61659765+johamja@users.noreply.github.com>
2021-03-08 11:41:24 -0500

    Update joham.js

--- joham.js
+++ joham.js
@@ -4,24 +4,20 @@ const PORT = process.env.PORT || 5000
 
 express()
 
-   /*
+   
    // estamos cargando la carpeta pero de manera estatica 
-   .use(express.static(path.join(__dirname, 'public')))
+   .use(express.static(path.join(__dirname, 'public/css')))
 
    // cargamos una carpeta normal
    .set('views', path.join(__dirname, 'view'))
 
      //iniciando el archivo de en /views/pagues/index.ejs
-     */ 
-
-     use(express.static("public"));
-
-     use(express.static("view"));
-
+     
 /* vamos a iniciar el archivo html */
    .get("/", (req, res) =>{
    res.sendFile(path.resolve(__dirname,"view/inicio.html"))
 })
+
 
   // iniciamos el port de heroku
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
