933c760c955d848a37a5f0c19e37fe341fe3db6c
johamja <61659765+johamja@users.noreply.github.com>
2021-03-08 11:21:19 -0500

    Update joham.js

--- joham.js
+++ joham.js
@@ -11,7 +11,11 @@ express()
    .set('views', path.join(__dirname, 'view'))
 
      //iniciando el archivo de en /views/pagues/index.ejs
-  .get('/', (req, res) => res.render('view/inicio.html'))
+     
+/* vamos a iniciar el archivo html */
+   .get("/", (req, res) =>{
+   res.sendFile(path.resolve(__dirname,"view/inicio.html"))
+})
 
   // iniciamos el port de heroku
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
