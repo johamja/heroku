696bb5cc6afca1645acc079d19e907baa8e43dde
johamja <61659765+johamja@users.noreply.github.com>
2021-03-08 11:49:29 -0500

    Update joham.js

--- joham.js
+++ joham.js
@@ -6,17 +6,16 @@ express()
 
    
    // estamos cargando la carpeta pero de manera estatica 
-   .use(express.static(path.join(__dirname, 'public')))
+   .use(express.static(__dirname + 'public'))
 
    // cargamos una carpeta normal
-   .set('views', path.join(__dirname, 'view'))
-
+   .set('views',(__dirname + 'views'))
      //iniciando el archivo de en /views/pagues/index.ejs
      
-/* vamos a iniciar el archivo html */
+   /* vamos a iniciar el archivo html */
    .get("/", (req, res) =>{
    res.sendFile(path.resolve(__dirname,"view/inicio.html"))
-})
+  })
 
 
   // iniciamos el port de heroku
