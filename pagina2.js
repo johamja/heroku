d9ee257925f21ebff567d685eb9f46bbc2b24f99
johamja <61659765+johamja@users.noreply.github.com>
2021-03-09 12:43:22 -0500

    f

---
+++ pagina2.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+
+   
+   // estamos cargando la carpeta pero de manera estatica 
+   .use(express.static(__dirname + '/public'))
+
+   // cargamos una carpeta normal
+   .set('views',(__dirname + '/views'))
+     //iniciando el archivo de en /views/pagues/index.ejs
+     
+   /* vamos a iniciar el archivo html */
+   .get("/", (req, res) =>{
+   res.sendFile(path.resolve(__dirname,"view/pagina2.html"))
+  })
+
+
+  // iniciamos el port de heroku
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
